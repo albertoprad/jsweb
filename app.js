a9311255d9db8f8f04da79fba21e786c873c0153
Jose Ignacio <joserobleda@gmail.com>
2014-10-20 18:05:00 +0200

    routes, controllers and views

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var express = require('express');
+var express = require('express'); 
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('hello world');
+app.get('/', function (req, res) {
+	res.render('hello.twig', {
+		name: 'perry',
+	});
 });
 
-app.get('/settings', function(req, res) {
-	res.send('settings page');
+app.get('/:owner/:repo', function (req, res) {
+	var owner 	= req.params.owner;
+	var repo 	= req.params.repo;
+
+	res.render('project.twig', {
+		project: owner + '/' + repo,
+		owner: owner,
+		repo: repo
+	});
+});
+
+app.get('/settings', function (req, res) {
+	res.render('settings.twig');
 });
 
 var server = app.listen(8080, function () {
