a5a245766304715e258aa2d76ac499a5e31ee861
albertoprad <asanchez@dokify.net>
2014-10-20 18:05:29 +0200

    hello view

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var express = require('express');
-var app = express();
+var express		= require('express');
+var app 		= express();
 
-app.get('/', function(req, res){
-	res.send('Hello World');
+
+
+app.get('/', function (req, res) {
+	res.render('hello.twig', {
+		name: 'Jose'
+	});
 });
 
-app.get('/settings', function(req, res){
-	res.send('You are in user setting');
+app.get('/settings', function (req, res) {
+	res.render('settings.twig', {
+		name: 'Alberto'
+	});
 });
+
+app.get('/:owner/:repo', function (req, res) {
+	var owner 	= req.params.owner;
+	var repo 	= req.params.repo;
+
+	res.render ('project.twig', {
+		project: owner + '/' + repo,
+		owner: owner,
+		repo: repo
+	});
+});
+
+
 
 var server = app.listen(8080, function () {
 
