e0a1bfb50772324193c739b498978f6acdd50166
Jose Ignacio <joserobleda@gmail.com>
2014-10-20 17:21:49 +0200

    app.js first version

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('hello world');
+});
+
+app.get('/settings', function(req, res) {
+	res.send('settings page');
+});
+
+var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+});
